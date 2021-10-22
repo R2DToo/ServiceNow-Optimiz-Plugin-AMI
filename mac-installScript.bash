b3e242a82b9d8ad9031bf60ac046b125be98335e
Braden Still-Routley <bradenxd12@gmail.com>
2021-10-22 13:11:48 -0500

    Dashboard updates and new mac script

--- mac-installScript.bash
+++ mac-installScript.bash
@@ -1,15 +1,5 @@
 #!/bin/bash
-which -s brew
-if [[ $? != 0 ]]
-then
-    # Install Homebrew
-    echo "Homebrew was now found on your system. Proceeding to install it for you..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-else
-    echo "ITx Jedis - this may take a few minutes. Be patient and get your lightsabers ready ...."
-    brew update -v
-fi
-
+grafanaDir=$(echo $HOME)"/grafana"
 installGrafana="blank"
 while [ "$installGrafana" != "n" ] && [ "$installGrafana" != "y" ] && [ "$installGrafana" != "N" ] && [ "$installGrafana" != "Y" ]
 do
@@ -21,33 +11,43 @@ do
     read installGrafana
 done
 
-if [ "$installGrafana" == "y" ] || ["$installGrafana" == "Y"]
+
+
+if [ "$installGrafana" == "y" ] || [ "$installGrafana" == "Y" ]
 then
-        echo "Installing Grafana"
-        brew install grafana
-        brew link grafana
-        brew services start grafana
-        mkdir -p /usr/local/Cellar/grafana/8.2.1/data/plugins
-        cp -r novatec-sdg-panel /usr/local/Cellar/grafana/8.2.1/data/plugins/
-        cp -r servicenow-optimiz-plugin /usr/local/Cellar/grafana/8.2.1/data/plugins/
-        cp dashboards/mac-SNOWdashboards.yaml /usr/local/Cellar/grafana/8.2.1/conf/provisioning/dashboards/
-        mkdir -p /usr/local/Cellar/grafana/8.2.1/conf/provisioning/dashboards/SNOWdashboards
-        cp -r dashboards/* /usr/local/Cellar/grafana/8.2.1/conf/provisioning/dashboards/SNOWdashboards/
+        echo "What version of Grafana do you want to install? The default option is 8.2.2"
+        read grafanaVersion
+        grafanaVersion=${grafanaVersion:-8.2.2}
+        echo "Installing Grafana to $grafanaDir/grafana-$grafanaVersion"
+        mkdir -p $grafanaDir
+        curl -O https://dl.grafana.com/oss/release/grafana-$grafanaVersion.darwin-amd64.tar.gz
+        tar -zxvf grafana-$grafanaVersion.darwin-amd64.tar.gz
+        rm -rf grafana-$grafanaVersion.darwin-amd64.tar.gz
+        mv grafana-$grafanaVersion $grafanaDir/
+        grafanaDir=$grafanaDir/grafana-$grafanaVersion
+        mkdir -p $grafanaDir/data/plugins
+        cp -R novatec-sdg-panel $grafanaDir/data/plugins/
+        cp -R servicenow-optimiz-plugin $grafanaDir/data/plugins/
+        mkdir -p $grafanaDir/conf/provisioning/dashboards/SNOWdashboards
+        cp dashboards/mac-SNOWdashboards.yaml $grafanaDir/conf/provisioning/dashboards/
+        cp -R dashboards/* $grafanaDir/conf/provisioning/dashboards/SNOWdashboards/
         echo "Restarting Grafana"
-        brew services restart grafana
+        $grafanaDir/bin/grafana-server web
 else
-        mkdir -p /usr/local/Cellar/grafana/8.2.1/data/plugins
-        rm -rf /usr/local/Cellar/grafana/8.2.1/data/plugins/novatec-sdg-panel
-        cp -r novatec-sdg-panel /usr/local/Cellar/grafana/8.2.1/data/plugins/
-        rm -rf /usr/local/Cellar/grafana/8.2.1/data/plugins/servicenow-optimiz-plugin
-        cp -r servicenow-optimiz-plugin /usr/local/Cellar/grafana/8.2.1/data/plugins/
-        rm -rf /usr/local/Cellar/grafana/8.2.1/conf/provisioning/dashboards/mac-SNOWdashboards.yaml
-        cp dashboards/mac-SNOWdashboards.yaml /usr/local/Cellar/grafana/8.2.1/conf/provisioning/dashboards/
-        rm -rf /usr/local/Cellar/grafana/8.2.1/conf/provisioning/dashboards/SNOWdashboards
-        mkdir -p /usr/local/Cellar/grafana/8.2.1/conf/provisioning/dashboards/SNOWdashboards
-        cp -r dashboards/* /usr/local/Cellar/grafana/8.2.1/conf/provisioning/dashboards/SNOWdashboards/
+        echo "What version of Grafana are you using? The default option is 8.2.2"
+        read grafanaVersion
+        grafanaVersion=${grafanaVersion:-8.2.2}\
+        grafanaDir=$grafanaDir/grafana-$grafanaVersion
+        mkdir -p $grafanaDir/data/plugins
+        rm -rf $grafanaDir/data/plugins/novatec-sdg-panel
+        cp -R novatec-sdg-panel $grafanaDir/data/plugins/
+        rm -rf $grafanaDir/data/plugins/servicenow-optimiz-plugin
+        cp -R servicenow-optimiz-plugin $grafanaDir/data/plugins/
+        rm -rf $grafanaDir/conf/provisioning/dashboards/SNOWdashboards
+        mkdir -p $grafanaDir/conf/provisioning/dashboards/SNOWdashboards
+        rm -f $grafanaDir/conf/provisioning/dashboards/mac-SNOWdashboards.yaml
+        cp dashboards/mac-SNOWdashboards.yaml $grafanaDir/conf/provisioning/dashboards/
+        cp -R dashboards/* $grafanaDir/conf/provisioning/dashboards/SNOWdashboards/
         echo "Restarting Grafana"
-        brew services restart grafana
+        $grafanaDir/bin/grafana-server web
 fi
-grafanaDir=$(brew info grafana | grep /usr/local/)
-echo $grafanaDir
